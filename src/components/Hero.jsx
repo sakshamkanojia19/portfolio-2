4fc6e68c1d7efe8d4d339c682d4a9f15730e0721
Saksham Kanojia <sakshamkanojia2002@gmail.com>
2024-12-04 15:42:59 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import profilePic from "../assets/profilepic.png";
+import "./Hero.css";
+import { HERO_CONTENT } from "../constants";
+import { delay, motion, stagger } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transiton: { duration: 0.5,
+       staggerChildern: 0.5 ,
+      }
+  }
+}
+
+
+const childVariants = {
+  hidden:{opacity :0,x:-100},
+  visible : { opacity:1,x:0,transition:{duration:0.5}}
+}
+
+// real code
+const Hero = () => {
+  return (
+    <div className="pb-4 lg:mb-36">
+      <div className="flex flex-wrap lg:flex-row-reverse">
+        <div className="w-full lg:w-1/2">
+          <div className="flex justify-center lg:p-8 ">
+            {/* with framer motion below */}
+            <motion.img
+             src={profilePic} 
+             alt="saksham pic"
+             className="resize"
+            
+            initial ={{x: 100, opacity : 0}}
+            animate={{x:0,opacity:1}}
+            transition={{duration:1, delay:1.5}} />
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2">
+          <motion.div 
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants} className="flex flex-col items-center lg:items-start mt-10">
+            <motion.h2 
+            variants={childVariants}
+            className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+              Saksham Kanojia
+            </motion.h2>
+            <motion.span
+            variants={childVariants}
+             className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent  ">
+              Full Stack Developer
+            </motion.span>
+            <motion.p 
+            variants={childVariants}
+            className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+              {HERO_CONTENT}
+            </motion.p>
+
+            <motion.a
+            variants={childVariants}
+              href="https://drive.google.com/drive/folders/1F291vBTK2AzuqvlKGYP3suQex-3pj32s?usp=sharing"
+              target="_blank"
+              rel="noopener noreferre"
+              download
+              className="bg-white rounded-full p-4 text-sm text-black"
+            >
+              {" "}
+              Download Resume/CV ↓
+            </motion.a>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,94 @@
+import project1 from "../assets/projects/project-1.webp";
+import project2 from "../assets/projects/project-2.webp";
+import project3 from "../assets/projects/project-3.webp";
+import project4 from "../assets/projects/project-4.webp";
+
+export const HERO_CONTENT = `I am a passionate full-stack developer with expertise in React.js, Material-UI, Node.js, Express.js, and MongoDB. I specialize in creating efficient, user-friendly web applications and have a strong interest in blockchain and cybersecurity. My goal is to build secure, innovative systems while contributing to impactful projects, continuously learning, and delivering high-quality solutions that meet both user and business needs.`
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "Jan 2024 - Feb 2024",
+    role: "Generative AI & Data Science Trainee",
+    company: "IBM SkillBuild",
+    description: `Explored Generative AI with a focus on prompt engineering and advanced techniques. Developed insightful data visualizations using Data Science principles and tools like R and RStudio, enhancing analytical capabilities.`,
+    technologies: ["R", "RStudio", "Data Science", "Generative AI"],
+  },
+  {
+    year: "Sep 2023 - Nov 2023",
+    role: "Cybersecurity & Ethical Hacking Intern",
+    company: "Rinex.ai",
+    description: `Executed key cybersecurity tasks such as data gathering, vulnerability analysis, and network sniffing. Performed penetration testing using tools like NMAP, Wireshark, Zap, and Metasploit, ensuring secure coding standards. Conducted detailed vulnerability assessments and generated actionable reports using Zap.`,
+    technologies: ["NMAP", "Wireshark", "Burp Suite", "Zap", "Metasploit"],
+  },
+  {
+    year: "2023 - 2024",
+    role: "Major Project Lead (Decentralized Cryptocurrency Exchange DApp)",
+    company: "College Final Year Project",
+    description: `Led the development of a decentralized cryptocurrency exchange platform, focusing on seamless MetaMask wallet integration and optimized transaction management. Implemented token slippage functionality and enhanced swapping efficiency through Agile methodologies.`,
+    technologies: [
+      "React.js",
+      "Node.js",
+      "Solidity",
+      "Tailwind",
+      "Ethereum Blockchain",
+    ],
+  },
+];
+
+
+export const PROJECTS = [
+  {
+    title: "Portfolio Website",
+    image: project1,
+    description:
+      "Launched a personal portfolio website to showcase design projects, increasing client and employer engagement by 40%. Enhanced performance by 30% using Vite for faster builds and optimized delivery.",
+    technologies: ["HTML", "CSS", "JavaScript", "React.js", "Node.js", "Vite","Framer motion"],
+  },
+  {
+    title: "Modern Real Estate Website",
+    image: project2,
+    description:
+      "Deployed a responsive real estate platform allowing users to search and book properties. Integrated advanced search filters, location services with Leaflet, and user authentication with Auth0, reducing customer search time by 30% and increasing platform feasibility by 45%. Automation testing with Selenium improved fault tolerance by 20%.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React.js",
+      "Node.js",
+      "SQL",
+      "MongoDB",
+      "Prisma",
+      "Mantine UI",
+      "MERN stack",
+    ],
+    link:"https://sk-homes-phi.vercel.app/"
+  },
+  {
+    title: "Decentralized Cryptocurrency Exchange DApp",
+    image: project3,
+    description:
+      "Developed a decentralized cryptocurrency exchange platform with MetaMask wallet integration, improving user experience and reducing transaction time by 20%. Implemented token slippage functionality, optimizing trade fluctuation management by 15%, and enhanced swapping efficiency by 25% using Agile methodologies.",
+    technologies: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React.js",
+      "Node.js",
+      "API",
+      "Finance",
+      "Solidity",
+      "Ant-Design",
+      "Tailwind",
+      "Ethereum Blockchain",
+    ],
+    link:"https://bit-exchangex.netlify.app/"
+  },
+];
+
+
+export const CONTACT = {
+  address: "Delhi, India",
+  phoneNo: "+91 9310277188",
+  email: "sakshamkanojia2002@gmail.com",
+};
