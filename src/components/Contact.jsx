8afc88d9a8dfa62de1cab4fcea36790e1c432170
Saksham Kanojia <sakshamkanojia2002@gmail.com>
2025-01-12 20:27:41 +0530

    c74

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -139,7 +139,7 @@ const Contact = () => {
             className="mb-8 w-full appearance-none rounded-lg border border-gray-900 bg-transparent px-3 py-2 text-sm focus:border-gray-400 focus:outline-none"
           />
           {errors.name && (
-            <p className="text-sm text-pink-700">{errors.name}</p>
+            <p className="text-sm text-red-700">{errors.name}</p>
           )}
         </div>
 
@@ -178,7 +178,7 @@ const Contact = () => {
         {/* Submit Button */}
         <button
           type="submit"
-          className={`mb-8 w-full rounded bg-blue-400 px-4 py-2 text-sm font-semibold text-slate-950 hover:bg-purple-400 ${
+          className={`mb-8 w-full rounded bg-blue-600 px-4 py-2 text-sm font-semibold text-slate-950 hover:bg-blue-400 ${
             isSending ? "cursor-not-allowed opacity-50" : ""
           }`}
           disabled={isSending}
